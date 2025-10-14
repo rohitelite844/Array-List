ecf549b8af27ab563b00fa71d18835ae307a3970
Rohitt <rohitingle126@gmail.com>
2025-10-14 20:54:33 +0530

    first commit

---
+++ add.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class add {
+
+	public static void main(String[] args) {
+		ArrayList<String> colours=new ArrayList();
+		colours.add("Red");
+		colours.add("Green");
+		colours.add("Orange");
+		colours.add("White");
+		colours.add("Black");
+		System.out.println(colours);
+		
+
+	}
+
+}
