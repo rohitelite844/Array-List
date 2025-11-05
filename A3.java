3ed541927566680260b4eb479216ad9e767764a8
Rohit Ingle <rohitingle126@gmail.com>
2025-11-05 17:19:03 +0530

    Add files via upload

---
+++ A3.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class A3 {
+
+	public static void main(String[] args) {
+		ArrayList<String> colours=new ArrayList();
+		colours.add("RED");
+		colours.add("Green");
+		colours.add("Orange");
+		colours.add("Black");
+		colours.add("White");
+		colours.add(0,"Pink");
+		System.out.println(colours);
+		
+		
+	}
+
+}
