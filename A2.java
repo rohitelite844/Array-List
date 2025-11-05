72dc42382af7e410afc1ea6f876b1eaf65f5d6d7
Rohit Ingle <rohitingle126@gmail.com>
2025-11-05 17:16:32 +0530

    Add files via upload

---
+++ A2.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+public class A2 {
+
+	public static void main(String[] args) {
+		ArrayList<String> colours=new ArrayList();
+		colours.add("Red");
+		colours.add("Green");
+		colours.add("Orange");
+		colours.add("Blue");
+		colours.add("White");
+		
+		for(String Colour : colours){
+		System.out.println(Colour);
+		}
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
