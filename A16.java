a20689bbb746a43e1f4b161163a29b7aa65e4d26
Rohit Ingle <rohitingle126@gmail.com>
2025-11-10 15:51:11 +0530

    Add files via upload

---
+++ A16.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class A16 {
+
+	public static void main(String[] args) {
+		ArrayList<String> list1=new ArrayList();
+		list1.add("Red");
+		list1.add("Blue");
+		list1.add("Green");
+		list1.add("Pink");
+		System.out.println(list1);
+		
+		
+		System.out.println("list2"+list1.clone());
+		
+
+	}
+
+}
