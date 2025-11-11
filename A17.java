611dc926b622cf77717d68a618a1c1b9bc2e3884
Rohit Ingle <rohitingle126@gmail.com>
2025-11-11 15:00:16 +0530

    Add files via upload

---
+++ A17.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class A17 {
+
+	public static void main(String[] args) {
+		ArrayList<String> list1=new ArrayList();
+		list1.add("Red");
+		list1.add("Blue");
+		list1.add("Green");
+		list1.add("Pink");
+		System.out.println(list1);
+		
+		list1.removeAll(list1);
+		System.out.println("After Remove the list");
+		System.out.println(list1);
+	}
+
+}
